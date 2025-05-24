e4f978899bdb1e13d6dddeee556c6a1bb7e97f7c
Donald Ray <syntaxray@gmail.com>
2025-05-24 14:38:46 +0100

    Add copy email feature

--- scripts/home.js
+++ scripts/home.js
@@ -34,6 +34,17 @@ function close(item) {
 
 }
 
+function emailCopy() {
+    navigator.clipboard.writeText(EMAIL);
+}
+
+function copiedAlert() {
+    let snackbar = document.getElementById("snackbar");
+    snackbar.className = "show";
+    // After 0.5 seconds, remove the show class from div
+    setTimeout(() => { snackbar.className = snackbar.className.replace("show", ""); }, 500);
+}
+
 
 function contactButtonClicked (contactButton) {
     switch (contactButton) {
@@ -48,6 +59,20 @@ function contactButtonClicked (contactButton) {
                 popUpText.style.fontFamily = `"Viga", sans-serif`;
                 popUpText.style.fontSize = "1.1rem";
 
+
+                let copyIcon = document.createElement("span");
+                copyIcon.innerHTML = 
+                    `<svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 30H8C6.93913 30 5.92172 29.5786 5.17157 28.8284C4.42143 28.0783 4 27.0609 4 26V8C4 6.93913 4.42143 5.92172 5.17157 5.17157C5.92172 4.42143 6.93913 4 8 4H26C27.0609 4 28.0783 4.42143 28.8284 5.17157C29.5786 5.92172 30 6.93913 30 8V10M22 18H40C42.2091 18 44 19.7909 44 22V40C44 42.2091 42.2091 44 40 44H22C19.7909 44 18 42.2091 18 40V22C18 19.7909 19.7909 18 22 18Z" 
+                        stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>`;
+                copyIcon.classList.add("copy-svg");
+                copyIcon.style.cursor = "pointer";
+                copyIcon.addEventListener('click', () => {
+                    emailCopy();
+                    copiedAlert();
+                });
+
                 let closeIcon = document.createElement("span");
                 closeIcon.innerHTML = 
                         `<svg width="32" height="32" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
@@ -61,6 +86,7 @@ function contactButtonClicked (contactButton) {
                 
 
                 contactPopUp.appendChild(popUpText);
+                contactPopUp.appendChild(copyIcon);
                 contactPopUp.appendChild(closeIcon);
             }
             break;
