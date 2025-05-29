cbca543d9a13fd29cb7bd0366516999f5987bb16
Donald Ray <syntaxray@gmail.com>
2025-05-29 11:29:23 +0100

    Add top projects section

---
+++ scripts/project.js
@@ -0,0 +1,36 @@
+const topProjects = [
+    {
+        "id": 1,
+        "title": "Data Migration Solution",
+        "description": `"Migrate your data between data stores with ease!"`,
+        "code": "https://github.com/syntax-ray/demo-f_e-migration",
+        "demo": {
+            "hasDemo": false,
+            "link": undefined
+        },
+        "website": {
+            "hasWebsite": false,
+            "link": undefined
+        },
+        "tools": ["Python", "SQL", "Docker", "PostgreSQL", "MSSQL"]
+    },
+    {
+        id: 2,
+        "title": "Portfolio Website",
+        "description": `"Get to know me and see what I can do!"`,
+        "code": "https://github.com/syntax-ray/portfolio",
+        "demo": {
+            "hasDemo": false,
+            "link": undefined
+        },
+        "website": {
+            "hasWebsite": true,
+            "link": "https://syntax-ray.github.io/portfolio/"
+        },
+        "tools": ["HTML", "CSS", "Javascript", "Github Pages"]
+    },
+
+]
+
+
+export default topProjects;
