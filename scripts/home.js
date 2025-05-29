f6c28426eb673118df0eddd12ef4629309adf29f
Donald Ray <syntaxray@gmail.com>
2025-05-29 11:36:18 +0100

    Attempt to fix prod

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,36 @@
-import topProjects  from "./project.js";
+const topProjects = [
+    {
+        "id": 1,
+        "title": "Data Migration Solution",
+        "description": `"Migrate your data between data stores with ease!"`,
+        "code": "https://github.com/syntax-ray/demo-f_e-migration",
+        "demo": {
+            "hasDemo": false,
+            "link": undefined
+        },
+        "website": {
+            "hasWebsite": false,
+            "link": undefined
+        },
+        "tools": ["Python", "SQL", "Docker", "PostgreSQL", "MSSQL"]
+    },
+    {
+        id: 2,
+        "title": "Portfolio Website",
+        "description": `"Get to know me and see what I can do!"`,
+        "code": "https://github.com/syntax-ray/portfolio",
+        "demo": {
+            "hasDemo": false,
+            "link": undefined
+        },
+        "website": {
+            "hasWebsite": true,
+            "link": "https://syntax-ray.github.io/portfolio/"
+        },
+        "tools": ["HTML", "CSS", "Javascript", "Github Pages"]
+    },
+
+]
 
 let REGULAR_TXT_CLR = "#0A8266";
 let EMAIL = "okoth46@gmail.com";
